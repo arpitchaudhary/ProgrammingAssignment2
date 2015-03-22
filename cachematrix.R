64e78977223fbf0e114f297eb7ca64d9400c0bba
Arpit Chaudhary <arpitchaudhary52@gmail.com>
2015-03-22 17:47:48 +0530

    Assignment completed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script has 2 functions that would help cache inverse of a matrix. This would help if the matrix is large
+## for which inverse needs to be calculated.
 
-## Write a short comment describing this function
+## makeCacheMatrix function takes a matrix as an input and stores the matrix in a different environment. It also
+## sets the inverse of the matrix to null. This function returns a list that contains 4 functions that can be used
+## to set/get the matrix and the inverse of the matrix.
+## for eg: For getting the Inverse-
+##
+## y<-makeCacheMatrix(yourMatrix)
+## y$getInverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverse <- NULL
+      set <- function(y) {
+            x <<- y
+            inverse <<- NULL
+      }
+      get <- function() x
+      setInverse <- function(z) inverse<<-z
+      getInverse <- function() inverse
+      list(set = set, get = get,
+            setInverse = setInverse,
+            getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function is the core function that implements the caching mechanism. This function checks whether the
+## inverse is already calculated. If the inverse is already calculated then it returns the already calculated inverse
+## otherwise it calculates the inverse and caches the inverse. This method needs the list as input that is provided by
+## makeCacheMatrix function defined above in this script.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inverse <- x$getInverse()
+      if(!is.null(inverse)) {
+            message("getting cached inverse")
+            return(inverse)
+      }
+      matrix <- x$get()
+      inverse <- solve(matrix, ...)
+      x$setInverse(inverse)
+      inverse
 }
